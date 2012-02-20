a4c5387a553c046c0c41db41ac83e4b6602b5267
Victoriano Navarro Aguilar <vitiman@starfox.local>
2012-02-20 21:07:18 +0100

    Landing page de espera, errores 404 y 500

--- urls.py
+++ urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import direct_to_template
+
 
 # Uncomment the next two lines to enable the admin:
-#from django.contrib import admin
-#admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 #elections_patterns = patterns('elections.views',
 #
@@ -19,18 +21,20 @@ urlpatterns = patterns('',
     
     # Examples:
 
-    url(r'^$', 'elections.views.index', name='index'),
-    url(r'^login/', 'elections.views.login_user', name='login_user'),
-    url(r'^register/', 'elections.views.register', name='register'),
-    url(r'^publish/', 'elections.views.publish', name='publish'),
-    url(r'^candidate/(?P<id>\d+)/$', 'elections.views.candidate', name='candidate'),
-    url(r'^candidate/', 'elections.views.candidate', name='candidate'),
-    url(r'^logout/','elections.views.logout_user', name='logout_user'),
+    #url(r'^$', 'elections.views.index', name='index'),
+    #url(r'^login/', 'elections.views.login_user', name='login_user'),
+    #url(r'^register/', 'elections.views.register', name='register'),
+    #url(r'^publish/', 'elections.views.publish', name='publish'),
+    #url(r'^candidate/(?P<id>\d+)/$', 'elections.views.candidate', name='candidate'),
+    #url(r'^candidate/', 'elections.views.candidate', name='candidate'),
+    #url(r'^logout/','elections.views.logout_user', name='logout_user'),
+    url(r'^$', direct_to_template, {'template': 'soon.html'}),
+    
     # url(r'^hr_elections/', include('hr_elections.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     #url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )

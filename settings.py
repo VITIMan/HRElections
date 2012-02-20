b082b33ab517f97fe3b9bb32c3e68e2c8a0967c1
Victoriano Navarro <victoriano.navarro@mon-digital.es>
2012-02-20 15:09:27 +0100

    settings.py fix en static

--- settings.py
+++ settings.py
@@ -56,7 +56,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/home/vitiman/webapps/static'
 
 STATICFILES_DIRS=('/home/vitiman/webapps/static',)
 # URL prefix for static files.

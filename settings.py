4ea627796556023c7f85ec96318a324b7a491bce
Victoriano Navarro <victoriano.navarro@mon-digital.es>
2012-02-20 16:02:53 +0100

    Cuestiiones de urls

--- settings.py
+++ settings.py
@@ -120,10 +120,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    # Uncomment the next line to enable the admin:
-    'django.contrib.admin',
     'django.contrib.staticfiles',
     'elections',
+    # Uncomment the next line to enable the admin:
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
--- urls.py
+++ urls.py
@@ -4,28 +4,28 @@ from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-elections_patterns = patterns('hr_elections.elections.views',
+#elections_patterns = patterns('elections.views',
+#
+#    (r'^$', 'index'),
+#    (r'^login/', 'login_user'),
+#    (r'^register/','register'),
+#    (r'^publish/', 'publish'),
+#    (r'^candidate/(?P<id>\d+)/$', 'candidate'),
+#    (r'^candidate/', 'candidate'),
+#    (r'^logout/','logout_user'),
+#)
 
-    (r'^$', 'index'),
-    (r'^login/', 'login_user'),
-    (r'^register/','register'),
-    (r'^publish/', 'publish'),
-    (r'^candidate/(?P<id>\d+)/$', 'candidate'),
-    (r'^candidate/', 'candidate'),
-    (r'^logout/','logout_user'),
-)
-
-urlpatterns = elections_patterns + patterns('',
+urlpatterns = patterns('',
     
     # Examples:
 
-    #url(r'^$', 'elections.views.index', name='index'),
-    #url(r'^login/', 'elections.views.login_user', name='login_user'),
-    #url(r'^register/', 'elections.views.register', name='register'),
-    #url(r'^publish/', 'elections.views.publish', name='publish'),
-    #url(r'^candidate/(?P<id>\d+)/$', 'elections.views.candidate', name='candidate'),
-    #url(r'^candidate/', 'elections.views.candidate', name='candidate'),
-    #url(r'^logout/','elections.views.logout_user', name='logout_user'),
+    url(r'^$', 'elections.views.index', name='index'),
+    url(r'^login/', 'elections.views.login_user', name='login_user'),
+    url(r'^register/', 'elections.views.register', name='register'),
+    url(r'^publish/', 'elections.views.publish', name='publish'),
+    url(r'^candidate/(?P<id>\d+)/$', 'elections.views.candidate', name='candidate'),
+    url(r'^candidate/', 'elections.views.candidate', name='candidate'),
+    url(r'^logout/','elections.views.logout_user', name='logout_user'),
     # url(r'^hr_elections/', include('hr_elections.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:

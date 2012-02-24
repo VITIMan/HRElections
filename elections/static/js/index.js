e932540d4379ca9685b576b872a9501cbd8e6cd8
Victoriano Navarro Aguilar <vitiman@starfox.local>
2012-02-24 20:42:04 +0100

    El carrousel se para al pasar el cursor del ratón y se libera al sacarlo

--- elections/static/js/index.js
+++ elections/static/js/index.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
         $('#carou').carousel();
+    $('#carou').mouseenter(function() {
+        $(this).carousel('pause');
+    }).mouseleave(function() {
+        $(this).carousel('next');
+    }); 
+      
+
+        
 });
